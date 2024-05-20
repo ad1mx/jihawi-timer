6d5ccf77d18ae7e271f738202e17ad5d58da936c
Ad1mx <hedrog33n@gmail.com>
2024-05-20 21:23:25 +0100

    updates

--- src/app/(root)/timer/page.tsx
+++ src/app/(root)/timer/page.tsx
@@ -1,3 +1,4 @@
+import { AiFillClockCircle, AiFillCalendar } from "@/icons";
 import hd from "humanize-duration";
 import { Nunito, Tajawal } from "next/font/google";
 import React from "react";
@@ -6,43 +7,78 @@ const tajawal = Tajawal({ weight: ["700"], subsets: ["arabic"] });
 const nunito = Nunito({ weight: ["700"], subsets: ["latin"] });
 
 const TimerPage = () => {
-  const jihawiDate = new Date(2024, 4, 5, 0, 0, 0).getTime();
-  const now = new Date().getTime();
-  const remainTime = jihawiDate - now;
+  const jihawiDate = new Date(2024, 5, 5, 0, 0, 0);
+  const remainTime = jihawiDate.getTime() - Date.now();
 
   return (
-    <main className="h-full flex flex-col justify-center text-center px-4">
+    <main className="h-full flex flex-col justify-center px-4 items-center">
       <header className={`${tajawal.className} text-3xl flex flex-col`}>
         <span className={`${nunito.className}`}>Time remaing to JIHAWI</span>
         <span>الوقت المتبقي للجهوي</span>
       </header>
       {/* EN */}
-      <p
-        className={`mt-8 text-3xl font-semibold text-green-400 ${nunito.className}`}
-      >
-        {hd(remainTime, { units: ["w", "d", "h"], round: true })} (
-        {hd(remainTime, { units: ["d"], round: true })})
-      </p>
+      <div className="mt-8 flex text-green-400">
+        <AiFillClockCircle className="text-4xl text-green-300" />
+        <p className={`ml-2 text-3xl font-semibold ${nunito.className}`}>
+          {hd(remainTime, { units: ["w", "d", "h"], round: true })} (
+          {hd(remainTime, { units: ["d"], round: true })})
+        </p>
+      </div>
       {/* AR */}
-      <p
-        dir="rtl"
-        className={`mt-2 text-3xl font-semibold text-green-400 ${tajawal.className}`}
-      >
-        {hd(remainTime, {
-          units: ["w", "d", "h"],
-          language: "ar",
-          digitReplacements: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-          round: true,
-        })}{" "}
-        (
-        {hd(remainTime, {
-          units: ["d"],
-          language: "ar",
-          digitReplacements: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-          round: true,
-        })}
-        )
-      </p>
+      <div dir="rtl" className="mt-2 flex text-green-400">
+        <AiFillClockCircle className="text-4xl text-green-300" />
+        <p
+          className={`mr-2 text-3xl font-semibold text-green-400 ${tajawal.className}`}
+        >
+          {hd(remainTime, {
+            units: ["w", "d", "h"],
+            language: "ar",
+            digitReplacements: [
+              "0",
+              "1",
+              "2",
+              "3",
+              "4",
+              "5",
+              "6",
+              "7",
+              "8",
+              "9",
+            ],
+            round: true,
+          })}{" "}
+          (
+          {hd(remainTime, {
+            units: ["d"],
+            language: "ar",
+            digitReplacements: [
+              "0",
+              "1",
+              "2",
+              "3",
+              "4",
+              "5",
+              "6",
+              "7",
+              "8",
+              "9",
+            ],
+            round: true,
+          })}
+          )
+        </p>
+      </div>
+      {/* DATE */}
+      <div className="mt-8 flex">
+        <AiFillCalendar className="text-4xl text-blue-300" />
+        <p
+          className={`ml-2 text-3xl font-semibold text-blue-400 ${tajawal.className}`}
+        >
+          {jihawiDate.toLocaleDateString("ar", {
+            dateStyle: "full",
+          })}
+        </p>
+      </div>
     </main>
   );
 };
---
+++ src/icons/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export function AiFillClockCircle(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth={0}
+      viewBox="0 0 1024 1024"
+      height="1em"
+      width="1em"
+      {...props}
+    >
+      <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm176.5 585.7l-28.6 39a7.99 7.99 0 0 1-11.2 1.7L483.3 569.8a7.92 7.92 0 0 1-3.3-6.5V288c0-4.4 3.6-8 8-8h48.1c4.4 0 8 3.6 8 8v247.5l142.6 103.1c3.6 2.5 4.4 7.5 1.8 11.1z" />
+    </svg>
+  );
+}
+
+export function AiFillCalendar(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth={0}
+      viewBox="0 0 1024 1024"
+      height="1em"
+      width="1em"
+      {...props}
+    >
+      <path d="M112 880c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V460H112v420zm768-696H712v-64c0-4.4-3.6-8-8-8h-56c-4.4 0-8 3.6-8 8v64H384v-64c0-4.4-3.6-8-8-8h-56c-4.4 0-8 3.6-8 8v64H144c-17.7 0-32 14.3-32 32v176h800V216c0-17.7-14.3-32-32-32z" />
+    </svg>
+  );
+}
