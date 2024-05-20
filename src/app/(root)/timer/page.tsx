ff3d74ad0cefd9566f991cbfcabc4257c28bf5f1
Ad1mx <hedrog33n@gmail.com>
2024-05-20 20:52:59 +0100

    updates 2

--- src/app/(root)/timer/page.tsx
+++ src/app/(root)/timer/page.tsx
@@ -6,7 +6,7 @@ const tajawal = Tajawal({ weight: ["700"], subsets: ["arabic"] });
 const nunito = Nunito({ weight: ["700"], subsets: ["latin"] });
 
 const TimerPage = () => {
-  const jihawiDate = new Date(2024, 5, 5, 0, 0, 0).getTime();
+  const jihawiDate = new Date(2024, 4, 5, 0, 0, 0).getTime();
   const now = new Date().getTime();
   const remainTime = jihawiDate - now;
 
@@ -16,11 +16,32 @@ const TimerPage = () => {
         <span className={`${nunito.className}`}>Time remaing to JIHAWI</span>
         <span>الوقت المتبقي للجهوي</span>
       </header>
+      {/* EN */}
       <p
-        className={`mt-5 text-3xl font-semibold text-green-400 ${nunito.className}`}
+        className={`mt-8 text-3xl font-semibold text-green-400 ${nunito.className}`}
       >
-        - {hd(remainTime, { units: ["w", "d", "h"], round: true })} (
+        {hd(remainTime, { units: ["w", "d", "h"], round: true })} (
         {hd(remainTime, { units: ["d"], round: true })})
+      </p>
+      {/* AR */}
+      <p
+        dir="rtl"
+        className={`mt-2 text-3xl font-semibold text-green-400 ${nunito.className}`}
+      >
+        {hd(remainTime, {
+          units: ["w", "d", "h"],
+          language: "ar",
+          digitReplacements: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+          round: true,
+        })}{" "}
+        (
+        {hd(remainTime, {
+          units: ["d"],
+          language: "ar",
+          digitReplacements: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+          round: true,
+        })}
+        )
       </p>
     </main>
   );
