d4b125e360a66430acc183b47845a0d4dc545707
Ad1mx <hedrog33n@gmail.com>
2024-05-22 23:38:48 +0100

    xxasd

---
+++ src/app/(main)/info/page.tsx
@@ -0,0 +1,13 @@
+import { Analytics } from "@vercel/analytics/react";
+import React from "react";
+
+const InfoPage = () => {
+  return (
+    <div>
+      <Analytics />
+      InfoPage
+    </div>
+  );
+};
+
+export default InfoPage;
